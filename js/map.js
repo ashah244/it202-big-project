823f5c18111083a91e189bf253e169cbd34b5634
Abhi Shah <ashah244@uic.edu>
2019-12-09 17:14:55 -0600

    Got nav bar to work

--- js/map.js
+++ js/map.js
@@ -3,11 +3,41 @@ $(document).ready(function() {
   function hideScreens() {
     $(".content").hide();
   }
+  //
+  // $(".nav-link").on("click", function(){
+  //   hideScreens();
+  //   var target = $(this).attr("href");
+  //   $(target).show();
+  // });
 
-  $(".nav-link").on("click", function(){
-    hideScreens();
-    var target = $(this).attr("href");
-    $(target).show();
+  window.mdc.autoInit();
+// mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+const drawer = document.querySelector('.mdc-drawer').MDCDrawer;
+// console.log(drawer);
+
+// open drawer on nav icon
+document.getElementsByClassName("mdc-top-app-bar__navigation-icon")[0].addEventListener("click", function(){
+  drawer.open = true;
+});
+
+
+// close drawer on selection
+//       let navs = document.getElementsByClassName("mdc-list-item");
+
+//       for (let i = 0; i < navs.length; i++) {
+//         navs[i].addEventListener("click", function() {
+//           drawer.open = false;
+//         })
+//       };
+
+  $("nav.mdc-list a.mdc-list-item").on("click", function(e) {
+     hideScreens();
+     let file = $(this).attr("href");
+     $(file).show();
+     drawer.open = false;
+
+
+
   });
 
   //variable for holding the query
--- js/search.js
+++ js/search.js
@@ -12,15 +12,46 @@ function clearInput(){
 
 $(document).ready(function() {
 
+  window.mdc.autoInit();
+// mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+const drawer = document.querySelector('.mdc-drawer').MDCDrawer;
+// console.log(drawer);
+
+// open drawer on nav icon
+document.getElementsByClassName("mdc-top-app-bar__navigation-icon")[0].addEventListener("click", function(){
+  drawer.open = true;
+});
+
+
+// close drawer on selection
+//       let navs = document.getElementsByClassName("mdc-list-item");
+
+//       for (let i = 0; i < navs.length; i++) {
+//         navs[i].addEventListener("click", function() {
+//           drawer.open = false;
+//         })
+//       };
   function hideScreens() {
     $(".content").hide();
   }
 
-  $(".nav-link").on("click", function(){
-    hideScreens();
-    var target = $(this).attr("href");
-    $(target).show();
+  $("nav.mdc-list a.mdc-list-item").on("click", function(e) {
+     hideScreens();
+     let file = $(this).attr("href");
+     $(file).show();
+     drawer.open = false;
+
+
+
   });
+
+
+
+  // $(".nav-link").on("click", function(){
+  //   hideScreens();
+  //   var target = $(this).attr("href");
+  //   $(target).show();
+  // });
 
 //searchs through json file
   $("#btnSearch").on("click", function(){
