99627f1feaf05833f347255a541c07b4da8078a6
Abhi Shah <ashah244@uic.edu>
2019-12-09 23:07:56 -0600

    Working on service worker

--- cache-polyfill.js
+++ cache-polyfill.js
@@ -16,137 +16,86 @@
  *  limitations under the License
  *
  */
-// if (!Cache.prototype.add) {
-//   Cache.prototype.add = function add(request) {
-//     return this.addAll([request]);
-//   };
-// }
-//
-// if (!Cache.prototype.addAll) {
-//   Cache.prototype.addAll = function addAll(requests) {
-//     var cache = this;
-//
-//     // Since DOMExceptions are not constructable:
-//     function NetworkError(message) {
-//       this.name = 'NetworkError';
-//       this.code = 19;
-//       this.message = message;
-//     }
-//     NetworkError.prototype = Object.create(Error.prototype);
-//
-//     return Promise.resolve().then(function() {
-//       if (arguments.length < 1) throw new TypeError();
-//
-//       // Simulate sequence<(Request or USVString)> binding:
-//       var sequence = [];
-//
-//       requests = requests.map(function(request) {
-//         if (request instanceof Request) {
-//           return request;
-//         }
-//         else {
-//           return String(request); // may throw TypeError
-//         }
-//       });
-//
-//       return Promise.all(
-//         requests.map(function(request) {
-//           if (typeof request === 'string') {
-//             request = new Request(request);
-//           }
-//
-//           var scheme = new URL(request.url).protocol;
-//
-//           if (scheme !== 'http:' && scheme !== 'https:') {
-//             throw new NetworkError("Invalid scheme");
-//           }
-//
-//           return fetch(request.clone());
-//         })
-//       );
-//     }).then(function(responses) {
-//       // TODO: check that requests don't overwrite one another
-//       // (don't think this is possible to polyfill due to opaque responses)
-//       return Promise.all(
-//         responses.map(function(response, i) {
-//           return cache.put(requests[i], response);
-//         })
-//       );
-//     }).then(function() {
-//       return undefined;
-//     });
-//   };
-// }
-//
-// if (!CacheStorage.prototype.match) {
-//   // This is probably vulnerable to race conditions (removing caches etc)
-//   CacheStorage.prototype.match = function match(request, opts) {
-//     var caches = this;
-//
-//     return this.keys().then(function(cacheNames) {
-//       var match;
-//
-//       return cacheNames.reduce(function(chain, cacheName) {
-//         return chain.then(function() {
-//           return match || caches.open(cacheName).then(function(cache) {
-//             return cache.match(request, opts);
-//           }).then(function(response) {
-//             match = response;
-//             return match;
-//           });
-//         });
-//       }, Promise.resolve());
-//     });
-//   };
-// }
+if (!Cache.prototype.add) {
+  Cache.prototype.add = function add(request) {
+    return this.addAll([request]);
+  };
+}
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
+if (!Cache.prototype.addAll) {
+  Cache.prototype.addAll = function addAll(requests) {
+    var cache = this;
+
+    // Since DOMExceptions are not constructable:
+    function NetworkError(message) {
+      this.name = 'NetworkError';
+      this.code = 19;
+      this.message = message;
+    }
+    NetworkError.prototype = Object.create(Error.prototype);
+
+    return Promise.resolve().then(function() {
+      if (arguments.length < 1) throw new TypeError();
+
+      // Simulate sequence<(Request or USVString)> binding:
+      var sequence = [];
+
+      requests = requests.map(function(request) {
+        if (request instanceof Request) {
+          return request;
         }
+        else {
+          return String(request); // may throw TypeError
+        }
+      });
 
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+        requests.map(function(request) {
+          if (typeof request === 'string') {
+            request = new Request(request);
           }
+
+          var scheme = new URL(request.url).protocol;
+
+          if (scheme !== 'http:' && scheme !== 'https:') {
+            throw new NetworkError("Invalid scheme");
+          }
+
+          return fetch(request.clone());
         })
       );
-    })
-  );
-});
+    }).then(function(responses) {
+      // TODO: check that requests don't overwrite one another
+      // (don't think this is possible to polyfill due to opaque responses)
+      return Promise.all(
+        responses.map(function(response, i) {
+          return cache.put(requests[i], response);
+        })
+      );
+    }).then(function() {
+      return undefined;
+    });
+  };
+}
+
+if (!CacheStorage.prototype.match) {
+  // This is probably vulnerable to race conditions (removing caches etc)
+  CacheStorage.prototype.match = function match(request, opts) {
+    var caches = this;
+
+    return this.keys().then(function(cacheNames) {
+      var match;
+
+      return cacheNames.reduce(function(chain, cacheName) {
+        return chain.then(function() {
+          return match || caches.open(cacheName).then(function(cache) {
+            return cache.match(request, opts);
+          }).then(function(response) {
+            match = response;
+            return match;
+          });
+        });
+      }, Promise.resolve());
+    });
+  };
+}
